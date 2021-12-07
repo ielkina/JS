98a4b610f58e6f14d7d67314acf04cdc5600ef4c
Iryna <ielkinairinka@gmail.com>
2021-12-07 20:33:26 +0300

    addet 008 script

---
+++ practice/008script.js
@@ -0,0 +1,8 @@
+/*Интерполяция*/
+"use strict";
+/*прямо внутри строки мы можем вставлять значение переменной*/
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
